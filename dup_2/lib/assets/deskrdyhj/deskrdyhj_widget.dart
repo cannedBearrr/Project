edc0900c084e0ad73af8413b7f36b106e03cd2a5
FTAA <ansonz@feitianacademy.org>
2024-01-22 14:56:23 -0500

    :) fasdfhiusdhfouase

--- dup_2/lib/assets/deskrdyhj/deskrdyhj_widget.dart
+++ dup_2/lib/assets/deskrdyhj/deskrdyhj_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -6,6 +8,8 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'deskrdyhj_model.dart';
 export 'deskrdyhj_model.dart';
 import 'package:indexed/indexed.dart';
+
+
 
 class DeskrdyhjWidget extends StatefulWidget {
   const DeskrdyhjWidget({super.key});
@@ -20,13 +24,25 @@ class _DeskrdyhjWidgetState extends State<DeskrdyhjWidget>
 
   double tV = 0.2;
   double tV_1 = 0.2;
+  double yV = 0.22;
+  double yV_1 = 0.22;
+  double uV = 0.2;
+  double uV_1 = 0.2;
+  double iV = 0.2;
+  double iV_1 = 0.2;
+
   int tlPri = 0;
   int trPri = 0;
   int blPri = 0;
   int brPri = 0;
   double _05to1tl = 0.5;
+  double _05to1tr = 0.5;
+  double _05to1bl = 0.5;
+  double _05to1br = 0.5;
 
-
+  // double lerp(double a, double b, double t) {
+  //   return lerp(a, b, t);
+  // }
 
   late final AnimationController _controller = AnimationController(
     duration: const Duration(seconds: 2),
@@ -83,7 +99,7 @@ class _DeskrdyhjWidgetState extends State<DeskrdyhjWidget>
                         child: TweenAnimationBuilder<double>(
                           curve: Curves.easeOutQuart,
                           tween: Tween<double>(begin: tV_1, end: tV,),
-                          duration: const Duration(milliseconds: 690),
+                          duration: const Duration(milliseconds: 600),
                           onEnd: () {
     if (tV == 0.2) {tlPri = 0; print("low"); _05to1tl = 0.5;}
                           },
@@ -91,7 +107,7 @@ class _DeskrdyhjWidgetState extends State<DeskrdyhjWidget>
                             return GestureDetector(
                               onTap: () {
                                 setState(() {
-                                  tV_1 = tV;
+                                  tV_1 = value;
                                   tV = tV == 0.2 ? 0.6 : 0.2;
                                   if (tV == 0.6) {tlPri = 1; print("hi"); _05to1tl = 1.0;}
                                   print(tV);
@@ -165,64 +181,83 @@ class _DeskrdyhjWidgetState extends State<DeskrdyhjWidget>
                 child: Positioned(
                   top: 0,
                   right: 0,
-                  width: MediaQuery.sizeOf(context).width * 0.5,
-                  height: MediaQuery.sizeOf(context).height * 0.5,
+                  width: MediaQuery.sizeOf(context).width * _05to1tr,
+                  height: MediaQuery.sizeOf(context).height * _05to1tr,
                   child: Padding(
                     padding: const EdgeInsetsDirectional.fromSTEB(0.0, 100.0, 100.0, 0.0),
-                    child: Container(
-                      width: MediaQuery.sizeOf(context).width * 0.5,
-                      height: MediaQuery.sizeOf(context).height * 0.5,
-                      decoration: const BoxDecoration(
-                        color: Color(0x00FFFFFF),
-                      ),
-                      child: Align(
-                        alignment: const AlignmentDirectional(1.0, -1.0),
+                    child: TweenAnimationBuilder<double>(
+                      curve: Curves.easeOutQuart,
+                      tween: Tween<double>(begin: yV_1, end: yV,),
+                      duration: const Duration(milliseconds: 600),
+                      onEnd: () {
+                        if (yV == 0.22) {trPri = 0; print("low1"); _05to1tr = 0.5;}
+                        print(_05to1tr);
+                        },
+                      builder: (BuildContext context, double value, Widget? child) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              yV_1 = value;
+                              yV = yV == 0.22 ? 0.5 : 0.22;
+                              if (yV == 0.5) {trPri = 1; print("hi1"); _05to1tr = 1.0;}
+                              print(yV);
+                            });
+                          },
                         child: Container(
-                          width: MediaQuery.sizeOf(context).width * 0.4,
-                          height: MediaQuery.sizeOf(context).height * 0.22,
                           decoration: const BoxDecoration(
-                            color: Color(0x00FFFFFF),
+                          color: Color(0x00FFFFFF),
+                          ),
+                          child: Align(
+                          alignment: const AlignmentDirectional(1.0, -1.0),
+                          child: Container(
+                          width: MediaQuery.sizeOf(context).width * (value+0.38),
+                          height: MediaQuery.sizeOf(context).height * value,
+                          decoration: const BoxDecoration(
+                          color: Color(0x00FFFFFF),
                           ),
                           child: Stack(
-                            children: [
-                              Align(
-                                alignment: const AlignmentDirectional(-1.0, 0.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(0.0),
-                                  child: Image.network(
-                                    'https://picsum.photos/seed/567/600',
-                                    width: double.infinity,
-                                    height: double.infinity,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              Align(
-                                alignment: const AlignmentDirectional(-1.0, 1.0),
-                                child: Padding(
-                                  padding: const EdgeInsetsDirectional.fromSTEB(
-                                      15.0, 0.0, 0.0, 15.0),
-                                  child: Text(
-                                    'NETWORKING',
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyMedium
-                                        .override(
-                                      fontFamily: 'Readex Pro',
-                                      color: Colors.white,
-                                      fontSize: 38.0,
-                                      letterSpacing: 6.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
+                          children: [
+                          Align(
+                          alignment: const AlignmentDirectional(-1.0, 0.0),
+                          child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0.0),
+                          child: Image.network(
+                          'https://picsum.photos/seed/567/600',
+                          width: double.infinity,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
                           ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                          ),
+                          ),
+                          Align(
+                          alignment: const AlignmentDirectional(-1.0, 1.0),
+                          child: Padding(
+                          padding: const EdgeInsetsDirectional.fromSTEB(
+                          15.0, 0.0, 0.0, 15.0),
+                          child: Text(
+                          'NETWORKING',
+                          style: FlutterFlowTheme.of(context)
+                              .bodyMedium
+                              .override(
+                          fontFamily: 'Readex Pro',
+                          color: Colors.white,
+                          fontSize: 38.0,
+                          letterSpacing: 6.0,
+                          fontWeight: FontWeight.bold,
+                          ),
+                          ),
+                          ),
+                          ),
+                          ],
+                          ),
+                          ),
+                          ),
+                          ),
+                          );
+                          },
+                          ),
+                          ),
+                          ),
               ),
 
               Indexed(
@@ -231,8 +266,8 @@ class _DeskrdyhjWidgetState extends State<DeskrdyhjWidget>
                 child: Positioned(
                   bottom: 0,
                   left: 0,
-                  width: MediaQuery.sizeOf(context).width * 0.5,
-                  height: MediaQuery.sizeOf(context).height * 0.5,
+                  width: MediaQuery.sizeOf(context).width * _05to1bl,
+                  height: MediaQuery.sizeOf(context).height * _05to1bl,
                   child: Padding(
                     padding: const EdgeInsetsDirectional.fromSTEB(100.0, 0.0, 0.0, 100.0),
                     child: Container(
@@ -317,8 +352,8 @@ class _DeskrdyhjWidgetState extends State<DeskrdyhjWidget>
                 child: Positioned(
                   bottom: 0,
                   right: 0,
-                  width: MediaQuery.sizeOf(context).width * 0.5,
-                  height: MediaQuery.sizeOf(context).height * 0.5,
+                  width: MediaQuery.sizeOf(context).width * _05to1br,
+                  height: MediaQuery.sizeOf(context).height * _05to1br,
                   child: Padding(
                     padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 100.0, 100.0),
                     child: Row(
