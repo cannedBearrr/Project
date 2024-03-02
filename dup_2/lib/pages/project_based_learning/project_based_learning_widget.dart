887e08f4bde5558a5f69221985aa35ae1b88e983
cannedBearrr <xixiseattle@icloud.com>
2024-03-01 19:40:45 -0500

    wadsad

--- dup_2/lib/pages/project_based_learning/project_based_learning_widget.dart
+++ dup_2/lib/pages/project_based_learning/project_based_learning_widget.dart
@@ -45,6 +45,11 @@ class _ProjectBasedLearningWidgetState
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
         body: Stack(
           children: [
+            if (responsiveVisibility(
+              context: context,
+              tablet: false,
+              phone: false,
+            ))
             ClipRRect(
               child: Image.network(
                 'https://picsum.photos/seed/542/600',
@@ -63,7 +68,6 @@ class _ProjectBasedLearningWidgetState
                   borderRadius: BorderRadius.circular(24.0),
                 ),
                 child: Column(
-
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
@@ -83,6 +87,57 @@ class _ProjectBasedLearningWidgetState
                     textAlign: TextAlign.center,
                     style: FlutterFlowTheme.of(context).bodyMedium.copyWith(
                       fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ]
+                      .divide(SizedBox(height: 40.0))
+                      .addToStart(SizedBox(height: 60.0)),
+                ),
+              ),
+            ),
+            if (responsiveVisibility(
+              context: context,
+              tablet: false,
+              phone: false,
+            ))
+              ClipRRect(
+                child: Image.network(
+                  'https://picsum.photos/seed/542/600',
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            Align(
+              alignment: AlignmentDirectional(0.0, 0.0),
+              child: Container(
+                width: MediaQuery.sizeOf(context).width * 0.8,
+                height: 800,
+                decoration: BoxDecoration(
+                  color: FlutterFlowTheme.of(context).secondaryBackground,
+                  borderRadius: BorderRadius.circular(24.0),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Project - Based Learning',
+                      textAlign: TextAlign.center,
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                        fontFamily: 'Readex Pro',
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(40.0, 0.0, 40.0, 0.0),
+                      child: Text(
+                        'Project-Based Learning (PBL) engages students in real-world projects, fostering critical thinking and collaboration. It mirrors authentic scenarios, promoting skills like inquiry-based learning and autonomy. Students actively contribute to project design, emphasizing the integration of subjects and the interconnectedness of real-world knowledge. PBL encourages reflection, self-assessment, and the development of essential skills. Assessment focuses on holistic growth, preparing students for the complexities of the real world.',
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.of(context).bodyMedium.copyWith(
+                          fontSize: 16.0,
                         ),
                       ),
                     ),
