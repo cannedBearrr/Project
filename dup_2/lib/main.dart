fee7436a7b97f6aa64d49b5070a7c82e3874f650
imacbig03 <charlie.cai@g.feitianacademy.org>
2024-03-01 23:40:34 -0500

    3-1 very very very lateasdfas

--- dup_2/lib/main.dart
+++ dup_2/lib/main.dart
@@ -1,76 +1,96 @@
+// Flutter dependencies
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// Custom FlutterFlow components and utilities
 import 'flutter_flow/flutter_flow_theme.dart';
 import 'flutter_flow/flutter_flow_util.dart';
 import 'flutter_flow/internationalization.dart';
 import 'flutter_flow/nav/nav.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+// Main application class
 void main() async {
+  // Initialize Flutter
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
 
+  // Initialize theme and set preferred orientations
   await FlutterFlowTheme.initialize();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
+  // Run the application
   runApp(const MyApp());
 }
 
+// Custom page transitions builder to disable transitions
 class NoTransitionsBuilder extends PageTransitionsBuilder {
   const NoTransitionsBuilder();
 
   @override
   Widget buildTransitions<T>(
-    PageRoute<T>? route,
-    BuildContext? context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget? child,
-  ) {
+      PageRoute<T>? route,
+      BuildContext? context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget? child,
+      ) {
     return child!;
   }
 }
 
+// Main application widget
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Root of the application
   @override
   State<MyApp> createState() => _MyAppState();
 
-  static _MyAppState of(BuildContext context) => context.findAncestorStateOfType<_MyAppState>()!;
+  // Static method to access the state from the context
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
 }
 
+// State class for the main application widget
 class _MyAppState extends State<MyApp> {
+  // Variables for locale, theme mode, app state notifier, and router
   Locale? _locale;
   ThemeMode _themeMode = FlutterFlowTheme.themeMode;
-
   late AppStateNotifier _appStateNotifier;
   late GoRouter _router;
 
+  // Initialize the state
   @override
   void initState() {
     super.initState();
 
+    // Initialize app state notifier and router
     _appStateNotifier = AppStateNotifier.instance;
     _router = createRouter(_appStateNotifier);
   }
 
+  // Set the locale based on language
   void setLocale(String language) {
     setState(() => _locale = createLocale(language));
   }
 
+  // Set the theme mode
   void setThemeMode(ThemeMode mode) => setState(() {
-        _themeMode = mode;
-        FlutterFlowTheme.saveThemeMode(mode);
-      });
+    _themeMode = mode;
+    FlutterFlowTheme.saveThemeMode(mode);
+  });
 
+  // Build the main application
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      // App title and configurations
       title: 'Dup 2',
       localizationsDelegates: const [
         FFLocalizationsDelegate(),
@@ -80,35 +100,29 @@ class _MyAppState extends State<MyApp> {
       ],
       locale: _locale,
       supportedLocales: const [Locale('en', '')],
+
+      // Light theme configuration
       theme: ThemeData(
         brightness: Brightness.light,
         scrollbarTheme: const ScrollbarThemeData(),
-          textSelectionTheme: TextSelectionThemeData(selectionColor: Color(0x55245ffe)),
-        // pageTransitionsTheme: const PageTransitionsTheme(
-        //   builders: <TargetPlatform, PageTransitionsBuilder>{
-        //     TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-        //     TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-        //     TargetPlatform.linux: NoTransitionsBuilder(),
-        //     TargetPlatform.macOS: NoTransitionsBuilder(),
-        //     TargetPlatform.windows: NoTransitionsBuilder(),
-        //   },
-        // ),
+        textSelectionTheme: TextSelectionThemeData(
+          selectionColor: Color(0x55245ffe),
+        ),
       ),
+
+      // Dark theme configuration
       darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          scrollbarTheme: const ScrollbarThemeData(),
-          textSelectionTheme: TextSelectionThemeData(selectionColor: Color(0x55245ffe)),
-          // pageTransitionsTheme: const PageTransitionsTheme(
-          //   builders: <TargetPlatform, PageTransitionsBuilder>{
-          //     TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-          //     TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-          //     TargetPlatform.linux: NoTransitionsBuilder(),
-          //     TargetPlatform.macOS: NoTransitionsBuilder(),
-          //     TargetPlatform.windows: NoTransitionsBuilder(),
-          //   },
-          // ),
-          ),
+        brightness: Brightness.dark,
+        scrollbarTheme: const ScrollbarThemeData(),
+        textSelectionTheme: TextSelectionThemeData(
+          selectionColor: Color(0x55245ffe),
+        ),
+      ),
+
+      // Initial theme mode
       themeMode: ThemeMode.light,
+
+      // Router configuration
       routerConfig: _router,
     );
   }
