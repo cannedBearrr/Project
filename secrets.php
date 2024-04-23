3ec8182e6d7aafd98c117d54e9050242c30521aa
anson-zhao <ansonzmj@gmail.com>
2024-04-23 18:12:12 -0400

    Some bug fixed

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51P8kCGRrAzkczPzUPqkttB4Wu2Oxl1KmrZlT3xiagvkI4Vi6loWmJWJ8QxAwkXq2VzgaDWsjbvCVj75Efz3d8DPY00wgSwFBfW';
