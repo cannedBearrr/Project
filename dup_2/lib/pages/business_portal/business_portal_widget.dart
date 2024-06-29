cfafc1a18f649e1d04d2c7f5592f5e48290cb60f
Yuvraj <ysmundi007@gmail.com>
2024-06-29 06:14:55 -0400

    asdf 6-29

--- dup_2/lib/pages/business_portal/business_portal_widget.dart
+++ dup_2/lib/pages/business_portal/business_portal_widget.dart
@@ -898,24 +898,7 @@ class _BusinessPortalWidgetState extends State<BusinessPortalWidget>
                                               child: Column(
                                                 children: [
                                                   Text(
-                                                    'Proin in elit diam. Nulla molestie \nmagna in elit laoreet, a tincidunt \nodio varius. Fusce non suscipit \nquam. Nam cursus efficitur erat ac \nposuere. Cras vel neque lectus. \nSuspendisse ut accumsan dui, in \nplacerat metus. Vivamus dictum \ndiam sit amet suscipit lobortis. \nNullam aliquam leo non tellus \nplacerat ornare ac vel ex.',
-                                                    textAlign:
-                                                        TextAlign.justify,
-                                                    style: FlutterFlowTheme.of(
-                                                            context)
-                                                        .bodyMedium
-                                                        .override(
-                                                          fontFamily:
-                                                              'Inria Serif',
-                                                          fontSize: 14.0,
-                                                          letterSpacing: 0.0,
-                                                        ),
-                                                  ),
-                                                  SizedBox(
-                                                      height:
-                                                          10.0), // Add spacing between the two text sections if needed
-                                                  Text(
-                                                    'Proin in elit diam. Nulla molestie \nmagna in elit laoreet, a tincidunt \nodio varius. Fusce non suscipit \nquam. Nam cursus efficitur erat ac \nposuere. Cras vel neque lectus. \nSuspendisse ut accumsan dui, in \nplacerat metus. Vivamus dictum \ndiam sit amet suscipit lobortis. \nNullam aliquam leo non tellus \nplacerat ornare ac vel ex.',
+                                                    'Northern Horizon (NH) stands out for your educational needs with personalized career counseling, skill development programs, industry exposure, and mentorship. Led by President Anson Zhao, with over 30 years of diverse corporate experience, NH empowers students to become leaders and innovators. We are not just an institution; we are a pathway to realizing potential into real-world achievements.',
                                                     textAlign:
                                                         TextAlign.justify,
                                                     style: FlutterFlowTheme.of(
@@ -1048,7 +1031,7 @@ class _BusinessPortalWidgetState extends State<BusinessPortalWidget>
                                                   MainAxisAlignment.start,
                                               children: [
                                                 Text(
-                                                  'WHY US?2',
+                                                  'WHY US?',
                                                   style: FlutterFlowTheme.of(
                                                           context)
                                                       .bodyMedium
