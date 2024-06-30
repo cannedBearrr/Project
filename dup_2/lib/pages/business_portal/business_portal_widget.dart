6f865870c625bf10ab7194342f5fe0b047f50b7c
Yuvraj <ysmundi007@gmail.com>
2024-06-30 11:10:50 -0400

    6-30 card images 2.0

--- dup_2/lib/pages/business_portal/business_portal_widget.dart
+++ dup_2/lib/pages/business_portal/business_portal_widget.dart
@@ -3579,7 +3579,7 @@ class _BusinessPortalWidgetState extends State<BusinessPortalWidget>
                                     ClipRRect(
                                       borderRadius: BorderRadius.circular(0.0),
                                       child: Image.network(
-                                        'https://images.unsplash.com/photo-1716223996671-d834e85ccdc6?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                                        'https://images.unsplash.com/photo-1494386346843-e12284507169?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                                         width:
                                             MediaQuery.sizeOf(context).width *
                                                 1.0,
@@ -3626,7 +3626,7 @@ class _BusinessPortalWidgetState extends State<BusinessPortalWidget>
                                           borderRadius:
                                               BorderRadius.circular(0.0),
                                           child: Image.network(
-                                            'https://images.unsplash.com/photo-1716223996671-d834e85ccdc6?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                                            'https://images.unsplash.com/photo-1494386346843-e12284507169?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                                             width: MediaQuery.sizeOf(context)
                                                     .width *
                                                 1.0,
